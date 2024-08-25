c86aca6dd818f32eacd30b130a4da0e2a25e6e9c
12Mosch <mosch12@tutanota.de>
2024-08-25 23:20:02 +0200

    Enderite Block and Ore is now mine able.

--- src/main/java/de/mosch/allesmacher/AllesMacher.java
+++ src/main/java/de/mosch/allesmacher/AllesMacher.java
@@ -3,7 +3,6 @@ package de.mosch.allesmacher;
 import de.mosch.allesmacher.block.ModBlocks;
 import de.mosch.allesmacher.item.ModCreativeModeTabs;
 import de.mosch.allesmacher.item.ModItems;
-import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -18,7 +17,6 @@ import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
